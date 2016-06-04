b14c3b8b1f2520461053a84c6e5b63c8cbb84b11
Kirill Nepomnyaschiy <nka1024@gmail.com>
2016-06-04 23:22:50 +0300

    parsing scalars

--- YAMLParser.swift
+++ YAMLParser.swift
@@ -8,11 +8,19 @@
 
 import yaml
 
+typealias scalar_t = yaml_event_t.__Unnamed_union_data.__Unnamed_struct_scalar
+
 public class YAMLParser {
+    
+    
+    // MARK: Initialization
     
     public init() {
         
     }
+
+    
+    // MARK: Version
     
     public func yamlGetVersionString() -> String? {
         let version = yaml_get_version_string()
@@ -29,6 +37,9 @@ public class YAMLParser {
         return (major, minor, patch)
     }
     
+    
+    // MARK: Stream parse
+    
     public func parseStream (path:String, callback: (data:String) -> Void){
         let mode = "rb"
         
@@ -44,29 +55,33 @@ public class YAMLParser {
         yaml_parser_set_input_file(&parser, fileHandler)
         
         repeat {
-            if (yaml_parser_parse(&parser, &event) == 0) {
-                print("Parser error %d\n", parser.error);
+            if yaml_parser_parse(&parser, &event) == 0 {
+                print("Parser error %d\n", parser.error)
                 exit(EXIT_FAILURE);
             }
             
             switch(event.type) {
-                case YAML_NO_EVENT: print("No event!"); break;
+                case YAML_NO_EVENT: print("No event!")
                 
                 /* Stream start/end */
-                case YAML_STREAM_START_EVENT: print("STREAM START"); break;
-                case YAML_STREAM_END_EVENT:   print("STREAM END");   break;
+                case YAML_STREAM_START_EVENT:   print("STREAM START")
+                case YAML_STREAM_END_EVENT:     print("STREAM END")
                 
                 /* Block delimeters */
-                case YAML_DOCUMENT_START_EVENT: print("Start Document");
-                case YAML_DOCUMENT_END_EVENT:   print("End Document");
-                case YAML_SEQUENCE_START_EVENT: print("Start Sequence");
-                case YAML_SEQUENCE_END_EVENT:   print("End Sequence");
-                case YAML_MAPPING_START_EVENT:  print("Start Mapping");
-                case YAML_MAPPING_END_EVENT:    print("End Mapping");
+                case YAML_DOCUMENT_START_EVENT: print("Start Document")
+                case YAML_DOCUMENT_END_EVENT:   print("End Document")
+                case YAML_SEQUENCE_START_EVENT: print("Start Sequence")
+                case YAML_SEQUENCE_END_EVENT:   print("End Sequence")
+                case YAML_MAPPING_START_EVENT:  print("Start Mapping")
+                case YAML_MAPPING_END_EVENT:    print("End Mapping")
                 
                 /* Data */
-                case YAML_ALIAS_EVENT:  print("Got alias (anchor %s)\n", event.data.alias.anchor);
-                case YAML_SCALAR_EVENT: print("Got scalar (value %s)\n", event.data.scalar.value);
+                case YAML_ALIAS_EVENT:
+                    print("Got alias (anchor: \(event.data.alias.anchor)");
+                case YAML_SCALAR_EVENT:
+                    
+                    print("Got scalar (value: \(scalarToString(event.data.scalar)))")
+                
                 
             default: print("Uknown event");
             }
@@ -78,9 +93,25 @@ public class YAMLParser {
         
         yaml_event_delete(&event);
         
-        /* Cleanup */
         yaml_parser_delete(&parser);
         fclose(fileHandler);
     }
 
+        
+    // MARK: Private routines
+    
+    func scalarToString(scalar:scalar_t) -> String? {
+        let len = scalar.length
+        let ptr = scalar.value
+    
+        return cStringToString(ptr, length: len)
+    }
+    
+    func cStringToString(ptr: UnsafeMutablePointer<UInt8>, length len: Int) -> String? {
+        if let theString = NSString(bytes: ptr, length: len, encoding: NSUTF8StringEncoding) {
+            return theString as String
+        } else {
+            return nil
+        }
+    }
 }
